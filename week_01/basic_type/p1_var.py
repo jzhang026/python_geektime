087e33fd554053efcbca5181744bf45f3ae99ac7
jiye zhang <jiyezhang@jiyedeMacBook-Pro.local>
2020-03-08 19:28:08 +0800

    summary of the third live cast

---
+++ week_01/basic_type/p1_var.py
@@ -0,0 +1,6 @@
+a = 123
+b = 123
+c = a
+print(id(a))
+print(id(b))
+print(id(c))
---
+++ week_01/debug.py
@@ -0,0 +1,12 @@
+def a():
+    return b()
+
+def b():
+    return c()
+
+def c():
+    return d()
+
+def d():
+    x = 0
+    return 100/x
