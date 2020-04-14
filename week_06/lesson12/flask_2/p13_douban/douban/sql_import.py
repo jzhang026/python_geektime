7395800757cb148e8087023b4215e26c263b9ba5
jiye zhang <jiyezhang@jiyedeMacBook-Pro.local>
2020-04-14 22:06:15 +0800

    save'

--- week_06/lesson12/flask_2/p13_douban/douban/sql_import.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('/tmp/result.csv') as f:
-    with open('result.sql','w',encoding='utf8') as g:    
-        reader = csv.reader(f)
-        for row in reader:
-            g.write(f"INSERT INTO t1 (n_star, short, sentiment) values ({row[0]}, '{row[1]}', {row[2]});\n")
-
